b3b95d150559769e4a618c5c24d1b1b14fb14c48
davidwushi1145 <david.wushi@foxmail.com>
2022-10-11 14:45:14 +0800

    My experiment2

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author Davidlee
+ * @version 1.0
+ * Create by 2022/10/11 14:43
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
